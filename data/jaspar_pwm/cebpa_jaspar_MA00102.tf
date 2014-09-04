9e957720e4132b5ec71727c0f63270dfe78648e4
Carl Herrmann <c.herrmann@dkfz.de>
2014-09-04 16:25:35 +0200

    included the JASPAR matrices for CEBPa

---
+++ data/jaspar_pwm/cebpa_jaspar_MA00102.tf
@@ -0,0 +1,119 @@
+AC  MA0102.1
+XX
+ID  Cebpa
+XX
+DE  MA0102.1 Cebpa; from JASPAR
+PO       A     C     G     T
+1        3     0     0    15
+2        1     1     4    12
+3        4     1     6     7
+4        2     9     2     5
+5        4     2    10     2
+6        2    15     0     1
+7       18     0     0     0
+8       18     0     0     0
+9        0     6     2    10
+10       5     6     5     2
+11       2     5     3     8
+12       1     6     5     6
+XX
+CC  program: jaspar
+CC  matrix.nb: 74
+CC  min.prior: 0.25
+CC  alphabet.size: 4
+CC  max.bits: 2
+CC  total.information: 5.47514146801893
+CC  information.per.column: 0.456261789001578
+CC  max.possible.info.per.col: 1.38629436111989
+CC  consensus.strict: TttcgCAAtctc
+CC  consensus.strict.rc: GAGATTGCGAAA
+CC  consensus.IUPAC: TtkygCAAyvyb
+CC  consensus.IUPAC.rc: VRBRTTGCRMAA
+CC  consensus.regexp: Tt[gt][ct]gCAA[ct][acg][ct][cgt]
+CC  consensus.regexp.rc: [ACG][AG][CGT][AG]TTGC[AG][AC]AA
+CC  residues.content.crude.freq: a:0.2778|c:0.2361|g:0.1713|t:0.3148
+CC  G+C.content.crude.freq: 0.407407407407407
+CC  residues.content.corrected.freq: a:0.2763|c:0.2368|g:0.1754|t:0.3114
+CC  G+C.content.corrected.freq: 0.412280701754386
+XX
+//
+
+AC  MA0102.2
+XX
+ID  CEBPA
+XX
+DE  MA0102.2 CEBPA; from JASPAR
+PO       A     C     G     T
+1        3     0     0    15
+2        1     1     4    12
+3        4     1     6     7
+4        2     9     2     5
+5        4     2    10     2
+6        2    15     0     1
+7       18     0     0     0
+8       18     0     0     0
+9        0     6     2    10
+XX
+CC  program: jaspar
+CC  matrix.nb: 142
+CC  min.prior: 0.25
+CC  alphabet.size: 4
+CC  max.bits: 2
+CC  total.information: 5.18369663231432
+CC  information.per.column: 0.575966292479369
+CC  max.possible.info.per.col: 1.38629436111989
+CC  consensus.strict: TttcgCAAt
+CC  consensus.strict.rc: ATTGCGAAA
+CC  consensus.IUPAC: TtkygCAAy
+CC  consensus.IUPAC.rc: RTTGCRMAA
+CC  consensus.regexp: Tt[gt][ct]gCAA[ct]
+CC  consensus.regexp.rc: [AG]TTGC[AG][AC]AA
+CC  residues.content.crude.freq: a:0.3210|c:0.2099|g:0.1481|t:0.3210
+CC  G+C.content.crude.freq: 0.358024691358025
+CC  residues.content.corrected.freq: a:0.3173|c:0.2120|g:0.1535|t:0.3173
+CC  G+C.content.corrected.freq: 0.365497076023392
+XX
+//
+
+
+AC  MA0102.3
+XX
+ID  CEBPA
+XX
+DE  MA0102.3 CEBPA; from JASPAR
+PO       A     C     G     T
+1    10351  1332  3635     0
+2        0     0     0 15318
+3        0     0     0 15318
+4      705     0 11304  3309
+5        0 15318     0     0
+6    11956  1274  1066  1022
+7     2660  7654     0  5004
+8    12226  3092     0     0
+9    15318     0     0     0
+10       0  5688  1804  7826
+11    5804  4963   890  3661
+XX
+CC  program: jaspar
+CC  matrix.nb: 219
+CC  min.prior: 0.25
+CC  alphabet.size: 4
+CC  max.bits: 2
+CC  total.information: 9.24140033536962
+CC  information.per.column: 0.84012730321542
+CC  max.possible.info.per.col: 1.38629436111989
+CC  consensus.strict: aTTGCAcAAta
+CC  consensus.strict.rc: TATTGTGCAAT
+CC  consensus.IUPAC: aTTGCAyAAym
+CC  consensus.IUPAC.rc: KRTTRTGCAAT
+CC  consensus.regexp: aTTGCA[ct]AA[ct][ac]
+CC  consensus.regexp.rc: [GT][AG]TT[AG]TGCAAT
+CC  residues.content.crude.freq: a:0.3503|c:0.2334|g:0.1110|t:0.3054
+CC  G+C.content.crude.freq: 0.344336431292953
+CC  residues.content.corrected.freq: a:0.3503|c:0.2334|g:0.1110|t:0.3054
+CC  G+C.content.corrected.freq: 0.344346592763591
+XX
+//
+
+
+
