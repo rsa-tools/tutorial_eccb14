79d9a39289e3f38e8929ce0d6947ac85e819f439
Carl Herrmann <c.herrmann@dkfz.de>
2014-09-04 22:56:36 +0200

    more on session 2

---
+++ data/jaspar_pwm/ma0102.1.tf
@@ -0,0 +1,39 @@
+AC  MA0102.1
+XX
+ID  Cebpa
+XX
+DE  MA0102.1 Cebpa; from JASPAR
+PO       A     C     G     T
+1        3     0     0    15
+2        1     1     4    12
+3        4     1     6     7
+4        2     9     2     5
+5        4     2    10     2
+6        2    15     0     1
+7       18     0     0     0
+8       18     0     0     0
+9        0     6     2    10
+10       5     6     5     2
+11       2     5     3     8
+12       1     6     5     6
+XX
+CC  program: jaspar
+CC  matrix.nb: 74
+CC  min.prior: 0.25
+CC  alphabet.size: 4
+CC  max.bits: 2
+CC  total.information: 5.47514146801893
+CC  information.per.column: 0.456261789001578
+CC  max.possible.info.per.col: 1.38629436111989
+CC  consensus.strict: TttcgCAAtctc
+CC  consensus.strict.rc: GAGATTGCGAAA
+CC  consensus.IUPAC: TtkygCAAyvyb
+CC  consensus.IUPAC.rc: VRBRTTGCRMAA
+CC  consensus.regexp: Tt[gt][ct]gCAA[ct][acg][ct][cgt]
+CC  consensus.regexp.rc: [ACG][AG][CGT][AG]TTGC[AG][AC]AA
+CC  residues.content.crude.freq: a:0.2778|c:0.2361|g:0.1713|t:0.3148
+CC  G+C.content.crude.freq: 0.407407407407407
+CC  residues.content.corrected.freq: a:0.2763|c:0.2368|g:0.1754|t:0.3114
+CC  G+C.content.corrected.freq: 0.412280701754386
+XX
+//
---
+++ data/jaspar_pwm/ma0102.2.tf
@@ -0,0 +1,36 @@
+AC  MA0102.2
+XX
+ID  CEBPA
+XX
+DE  MA0102.2 CEBPA; from JASPAR
+PO       A     C     G     T
+1        3     0     0    15
+2        1     1     4    12
+3        4     1     6     7
+4        2     9     2     5
+5        4     2    10     2
+6        2    15     0     1
+7       18     0     0     0
+8       18     0     0     0
+9        0     6     2    10
+XX
+CC  program: jaspar
+CC  matrix.nb: 142
+CC  min.prior: 0.25
+CC  alphabet.size: 4
+CC  max.bits: 2
+CC  total.information: 5.18369663231432
+CC  information.per.column: 0.575966292479369
+CC  max.possible.info.per.col: 1.38629436111989
+CC  consensus.strict: TttcgCAAt
+CC  consensus.strict.rc: ATTGCGAAA
+CC  consensus.IUPAC: TtkygCAAy
+CC  consensus.IUPAC.rc: RTTGCRMAA
+CC  consensus.regexp: Tt[gt][ct]gCAA[ct]
+CC  consensus.regexp.rc: [AG]TTGC[AG][AC]AA
+CC  residues.content.crude.freq: a:0.3210|c:0.2099|g:0.1481|t:0.3210
+CC  G+C.content.crude.freq: 0.358024691358025
+CC  residues.content.corrected.freq: a:0.3173|c:0.2120|g:0.1535|t:0.3173
+CC  G+C.content.corrected.freq: 0.365497076023392
+XX
+//
